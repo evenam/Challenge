b5bff1b63b0fe5d68d57c8844070bb53edeb80c6
Everett Moser <everettmoser@88-1f-a1-d-24-fe.wlan.lsu.edu>
2014-09-10 16:28:10 -0500

    friends function tested and works
    To the extent of my knowledge, friends function works

--- main.cpp
+++ main.cpp
@@ -4,21 +4,36 @@
 #include <algorithm>
 #include <iostream>
 
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
 std::vector<std::string> wordList;
 bool wordCompare(std::string word1, std::string word2);
+bool friends(std::string word1, std::string word2);
 
 int main(int argc, char** argv)
 {
 	std::ifstream infile;
-	std::string inputWord;
+	//std::string inputWord;
+	std::string iWord1, iWord2;
 	infile.open("randomlist.txt");
-	while (infile >> inputWord)
-		wordList.push_back(inputWord);
+	//while (infile >> inputWord)
+	//	wordList.push_back(inputWord);
 
-	std::sort(wordList.begin(), wordList.end(), wordCompare);
+	//std::sort(wordList.begin(), wordList.end(), wordCompare);
 
-	for (std::vector<std::string>::iterator i = wordList.begin(); i != wordList.end(); i ++)
-		std::cout << (*i) << std::endl;
+	//for (auto i = wordList.begin(); i != wordList.end(); i ++)
+	//	std::cout << (*i) << std::endl;
+	std::cin >> iWord1;
+	while (iWord1 != "exit")
+	{
+		std::cin >> iWord2;
+		if (friends(iWord1, iWord2))
+			std::cout << "FRIENDS!\n";
+		else
+			std::cout << "ENEMIES!\n";
+			
+		std::cin >> iWord1;
+	}
 
 	return 0;
 }
@@ -27,7 +42,31 @@ bool wordCompare(std::string word1, std::string word2)
 {
 	if (word1.size() < word2.size()) return true;
 	if (word2.size() < word1.size()) return false;
-	std::reverse(word1.begin(), word1.end());
-	std::reverse(word2.begin(), word2.end());
 	return (word1 < word2);
+}
+
+bool friends(std::string word1, std::string word2)
+{
+	if (abs(word1.size() - word2.size()) > 1)
+		return false;
+	
+	std::string tw1, tw2; // temp words for manipulating
+	// Testing words in reverse after sorting increases efficiency
+	for (int i = min(word1.size(), word2.size()) - 1; i >= 0; i --)
+	{	
+		tw1 = word1;
+		tw2 = word2;
+		tw1.erase(i, 1);
+		tw2.erase(i, 1);
+		std::cout << "TESTING:\t" << word1 << "\t" << word2 << "\n\t\t" 
+			<< tw1 << "\t" << tw2 << "\n";
+		if (word1 == tw2)
+			return true;
+		if (word2 == tw1)
+			return true;
+		if (tw1 == tw2)
+			return true;
+	}
+	
+	return false;
 }
